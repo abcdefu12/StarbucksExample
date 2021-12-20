2a8b09461b7887d8294fcff274149420f9fa0081
Gim <gbks081@gmail.com>
2021-12-20 14:52:39 +0900

    feat: visual section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // console.log('js'); //js연결확인위함
+
+// SEARCH //
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
@@ -15,3 +17,37 @@ searchInputEl.addEventListener('blur', function() {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+
+// BADGE //
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+    if(window.scrollY > 500){
+        gsap.to(badgeEl, .6, {
+            // name: '', age: ''
+            opacity: 0,
+            display: 'none'
+        });
+        
+        // badgeEl.style.display = 'none';
+    } else{
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+        // badgeEl.style.display = 'block';
+    }
+}, 300));
+// gsap.to(요소, 지속시간, 옵션); 애니메이션 라이브러리
+// _.throttle(함수, 시간) -> 이벤트에 부하를 걸어 데이터과부하 방지
+
+
+// FADE-IN
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index){
+    gsap.to(fadeEl, 1, {
+        delay: (index +1) * .7,
+        opacity: 1
+    });
+})
