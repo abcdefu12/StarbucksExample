265a9574f06d1b6a6165afa238701b4309963246
Gim <gbks081@gmail.com>
2021-12-20 17:24:50 +0900

    feat: notice section
    inside notice section, notice-line class & promotion class
    js: swiper & toggle

--- js/main.js
+++ js/main.js
@@ -51,3 +51,43 @@ fadeEls.forEach(function(fadeEl, index){
         opacity: 1
     });
 })
+
+// NOTICE SWIPER
+new Swiper('.notice-line .swiper',{
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});//new Swiper(선택자, 옵션)
+
+new Swiper('.promotion .swiper',{
+    // direction: 'horizontal' 기본값임
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    autoplay:{
+        delay: 5000
+    },
+    // pagination ->페이지 번호 요소 선택자
+    pagination: {
+        el: '.promotion .swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
+
+//TOGGLE PROMOTION
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function(){
+    isHidePromotion = !isHidePromotion
+    if(isHidePromotion){
+        promotionEl.classList.add('hide');
+    }else{
+        promotionEl.classList.remove('hide');
+    }
+});
